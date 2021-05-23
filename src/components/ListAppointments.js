5751255345cb06e7f2143f894093fb47cb23cdb5
Navdeep <beenavdeep@gmail.com>
2021-05-23 17:42:22 +0530

    state management with props

--- src/components/ListAppointments.js
+++ src/components/ListAppointments.js
@@ -1,10 +1,31 @@
-import React, {Component, component} from 'react';
+import React, {Component} from 'react';
 
 class ListAppointments extends Component {
     render () {
         return (
-            <div>List Appointments</div>
-        )
+            <div className="appointment-list item-list mb-3">
+                {this.props.appointments.map(apt => (
+                    <div className="pet-item col media py-3" key={apt.aptId}>
+                    <div className="mr-3">
+                      <button className="pet-delete btn btn-sm btn-danger">X</button>
+                    </div>
+        
+                    <div className="pet-info media-body">
+                      <div className="pet-head d-flex">
+                        <span className="pet-name">{apt.petName}</span>
+                        <span className="apt-date ml-auto">{apt.aptDate}</span>
+                      </div>
+        
+                      <div className="owner-name">
+                        <span className="label-item">Owner: </span>
+                        <span>{apt.ownerName}</span>
+                      </div>
+                      <div className="apt-notes">{apt.aptNotes}</div>
+                    </div>
+                  </div>
+                ))};
+        </div>
+        );
     }
 }
 
